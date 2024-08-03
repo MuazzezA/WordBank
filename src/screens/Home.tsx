706119c8cba2fd9c41c6d1f7d924d2acc8ef1122
Batuhan Orhan <bthnorhan00@gmail.com>
2024-08-04 02:12:59 +0300

    feat: text input

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {T} from '../components/T.tsx';
+import {WInput} from '../components/WInput.tsx';
 
 export const Home = () => {
   return (
     <SafeAreaView>
       <T text={'word bank'} size={18} />
+      <WInput placeholder="Test" weight="400" />
     </SafeAreaView>
   );
 };
